a3c41bd52398c821775f9dc67dd9d40a91c1a8d6
juan rene soler <juanr.soler_93@hotmail.es>
2018-04-10 00:52:34 -0400

    trabajando repositorios

---
+++ Economic.Core/Repositories/PrestamoDetalleRepository.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Economic.Core.Repositories
+{
+    public class PrestamoDetalleRepository : BaseRepository<Entities.Prestamo_Detalle>
+    {
+        public IEnumerable<Entities.Prestamo_Detalle> GetDetalleByIdMaster(int IdMaster)
+        {
+            return this.Select(x => x.Prestamo_Master_Id == IdMaster);
+        }
+
+        public void CalcularAmortizacion(int IdMaster)
+        {
+            var queryMaster = this.context.Prestamo_Master as IQueryable<Entities.Prestamo_Master>;
+
+            var master = (from a in queryMaster
+                          where a.Id == IdMaster
+                          select a
+                          ).FirstOrDefault();
+
+            //
+        }
+    }
+}
---
+++ Economic.Core/Repositories/PrestamoMasterRepository.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Economic.Core.Repositories
+{
+    public class PrestamoMasterRepository : BaseRepository<Entities.Prestamo_Master>
+    {
+        public Entities.Prestamo_Master GetMasterByIdEntidad(int IdEntidad)
+        {
+            return this.Find(x => x.PrestatarioId == IdEntidad);
+        }
+    }
+}
